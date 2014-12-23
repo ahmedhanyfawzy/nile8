867f2071140a2f7b6fe524c5224199fb7a1b0697
ahmedhanyfawzy <ahmedhanyfawzy@gmail.com>
2014-12-23 11:44:20 +0000

    add asset execlude

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
-
+config.assets.initialize_on_precompile = false
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -18,5 +18,6 @@ module Nile7
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
   end
+
 end
-config.assets.initialize_on_precompile = false
+
